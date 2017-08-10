915877e64bd29682e3734e647a2c1e1c9a8c295c
yaojin <sunyaojin2011@foxmail.com>
2017-08-10 19:13:37 -0400

    email customize

--- slackBot/src/test.py
+++ slackBot/src/test.py
@@ -5,7 +5,10 @@
 
 import smtplib
 from email.mime.text import MIMEText
-
+from email.utils import parseaddr, formataddr
+from email import encoders
+from email.header import Header
+import re
 
 
 
@@ -15,17 +18,38 @@ def readPassword():
 		passBin = p_out.read().strip('\n')
 	return passBin
 
- 
+def _format_addr(s):
+	name, addr = parseaddr(s)
+	return formataddr((\
+		Header(name,'utf-8').encode(),
+		addr.encode('utf-8') if isinstance(addr,unicode) else addr))
 
+def readEmailTemplate():
+	myEmailT_path = 'rctEmail.html'
+	with open(myEmailT_path,'r') as emailOUT:
+		emailContent = emailOUT.read()
+	return emailContent
 
+def customizeEmail(html_string):
+	newLine_PK_addr = '*****'+'\tPK\t'+'*****'
+	find_location = '\*\*\*\*\*\*\*\*\*\*\*\*'
+	newHTML = re.sub(find_location,newLine_PK_addr,html_string)
+	return newHTML
 
 
 if __name__ == '__main__':
-	msg = MIMEText('hello, send by Python','plain','utf-8')
+	# customizeEmail(readEmailTemplate())
+	msg = MIMEText(customizeEmail(readEmailTemplate()),'html','utf-8')
+
 	from_addr = "rctoken17@gmail.com"
 	to_addr = "foundation@rctoken.com"
 	passBin = readPassword()
 	smtp_server = 'smtp.gmail.com'
+
+	msg['From'] = _format_addr(u'RCT Developer Group <%s>'% from_addr)
+	msg['To'] = _format_addr(u'RCT supporters <%s>' % to_addr)
+	msg['Subject'] = Header(u'TEST email from RCT group','utf-8').encode()
+	
 	server = smtplib.SMTP(smtp_server,587)
 	server.ehlo()
 	server.starttls()
