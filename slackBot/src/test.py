faf3f620c62f0f1e6f246f04d4016abd8cc1c4ad
yaojin <sunyaojin2011@foxmail.com>
2017-08-10 13:33:57 -0400

    add key & qr code

--- slackBot/src/test.py
+++ slackBot/src/test.py
@@ -3,8 +3,10 @@
 
 
 
+
 import sha3
 from ecdsa import SigningKey, SECP256k1
+import qrcode
 
 keccak = sha3.keccak_256()
 private = SigningKey.generate(curve=SECP256k1)
@@ -13,6 +15,17 @@ keccak.update(public)
 address = "0x{}".format(keccak.hexdigest()[24:])
 
 
-print private.to_pem()
-print private.to_der()
+print(private.to_string().hex())
+print(address)
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(address)
+qr.make(fit=True)
 
+img = qr.make_image(fill_color="red", back_color="green")
+
+img.save('y.png')
