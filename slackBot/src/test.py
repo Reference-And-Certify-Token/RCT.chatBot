28e763a561090beaba80b7e3a14159afdda06969
yaojin <sunyaojin2011@foxmail.com>
2017-08-11 10:07:34 -0400

    sqlite

--- slackBot/src/test.py
+++ slackBot/src/test.py
@@ -3,16 +3,31 @@
 
 
 
+import sqlite3
+from verificationCode import code_generator
+
+conn = sqlite3.connect('/home/yaojin/.SlackFile/channel.db')
 
 
+cursor = conn.cursor()
+
+# cursor.execute('create table channelTable (channelId varchar(20) primary key, veriCode varchar(20))')
 
 
+my_channel = 'awddwd'
+my_veriCode = 'sfdssd'
 
+executeValue = '('+repr(code_generator())+','+repr(code_generator())+')'
+# executeValue = '('+repr(code_generator())+','+'None'+')'
+executeHead = 'INSERT INTO channelTable (channelId, veriCode) VALUES'
+executeFull = executeHead + executeValue
+print executeFull
 
+conn.execute(executeFull)
+conn.commit()
 
-
-
-
+for i in conn.execute('SELECT * FROM channelTable'):
+	print i
 
 
 
--- slackBot/src/verificationCode.py
+++ slackBot/src/verificationCode.py
@@ -6,7 +6,7 @@ import random
 
 def code_generator(size=8, chars=string.ascii_uppercase + string.digits):
 	myRandomString = ''.join(random.choice(chars) for _ in range(size))
-	print myRandomString
+	# print myRandomString
 	return myRandomString
 
 if __name__ == '__main__':
